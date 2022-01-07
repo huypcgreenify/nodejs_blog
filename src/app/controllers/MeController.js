c217b80ba8a6eaac271f887cde2478b6e56ce5e3
huypro123 <62203494+huypro123@users.noreply.github.com>
2022-01-07 19:33:10 +0700

    Delete_all

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,8 +4,37 @@ const { multipleMongoeseToObject } = require('../../util/mongoese')
 class MeController {
     //[GET] /stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', { courses: multipleMongoeseToObject(courses) }))
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) => //destructuring
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongoeseToObject(courses)
+                })
+            )
+            .catch(next)
+    }//Gộp 2 promisse ben dưới lên trên
+
+
+    //         Course.countDocumentsDeleted()
+    //             .then((deletedCount) => {
+    //               console.log(deletedCount)
+    //          })
+    //             .catch (() => { })
+    //          Course.find({})
+    //              .then(courses => res.render('me/stored-courses', {
+    //                   courses: multipleMongoeseToObject(courses)
+    //                  }))
+    //             .catch(next)
+    //     }
+
+
+    //[GET] /trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({})
+            .then(courses => res.render('me/trash-courses', {
+                courses: multipleMongoeseToObject(courses)
+            }),
+            )
             .catch(next)
 
     }
