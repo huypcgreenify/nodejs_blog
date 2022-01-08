210aea22d832e3bca561148ba4f3a6ae7417d19a
huypro123 <62203494+huypro123@users.noreply.github.com>
2022-01-08 19:28:28 +0700

    AllCheckBox_FixBug

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -60,6 +60,32 @@ class CourseController {
             .catch(next)
     }
 
+    //[POST] /courses/handle-form-action
+    handleFormActions(req, res, next) {
+        // res.json(req.body)
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next)
+                break
+
+            case 'restore':
+                Course.restore({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next)
+                break
+
+            case 'forceDelete':
+                Course.deleteOne({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect('back'))
+                    .catch(next)
+                break
+
+            default: res.json({ message: 'Action not allowed' })
+        }
+    }
+
 }
 
 module.exports = new CourseController
