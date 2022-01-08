6c4bb8ba9b37293ec0fd3cd881e0ce51bd27cc0b
huypro123 <62203494+huypro123@users.noreply.github.com>
2022-01-09 00:30:30 +0700

    update_render_html

--- src/app/Models/Course.js
+++ src/app/Models/Course.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator')
 const mongooseDelete = require('mongoose-delete')
 
-
-
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
